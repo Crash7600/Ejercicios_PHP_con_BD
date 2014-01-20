85f481d3d04f2bcd9d871e75c3b0f80df143121c
al037665 <al037665@s09-pc08>
2014-01-20 13:50:12 +0100

    Ejercicios PHP

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <h1>Ejercicios de PHP con Bases de Datos:</h1>
+        <a href="01CreateBD_BDWiley2.php">01CreateBD_BDWiley2.php</a>
+        <a href="02CreateMovie_BDWiley2.php">02CreateMovie_BDWiley2.php</a>
+        <a href="030MostrarCreacionTablas_BDWiley2.php">030MostrarCreacionTablas_BDWiley2.php</a>
+        <a href="04MovieData_BDWiley2.php">04MovieData_BDWiley2.php</a>
+        <a href="05InsertarPelis_BDWiley2.php">05InsertarPelis_BDWiley2.php</a>
+        <a href="06MostrarTablas_BDWiley2.php">06MostrarTablas_BDWiley2.php</a>
+        <a href="071InsertarPeli_BDWiley2.php">071InsertarPeli_BDWiley2.php</a>
+        <a href="072DeshabilitarRestricciones_BDWiley2.php">072DeshabilitarRestricciones_BDWiley2.php</a>
+        <a href="073InsertarPeli_BDWiley2.php">073InsertarPeli_BDWiley2.php</a>
+        <a href="074EliminaPeli_BDWiley2.php">074EliminaPeli_BDWiley2.php</a>
+        <a href="081HabilitarRestricciones_BDWiley2.php">081HabilitarRestricciones_BDWiley2.php</a>
+        <a href="082InsertarPeli_BDWiley2.php">082InsertarPeli_BDWiley2.php</a>
+    </body>
+</html>
