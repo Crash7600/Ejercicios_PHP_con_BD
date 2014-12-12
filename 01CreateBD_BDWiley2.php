b67e12e94a2b739215682ca4504118105eb3ee58
al037805 <al037805@s09-pc12>
2014-12-12 16:15:07 +0100

    Modificado fichero createBD_BDWilley2

--- 01CreateBD_BDWiley2.php
+++ 01CreateBD_BDWiley2.php
@@ -6,15 +6,12 @@ $conn=mysql_connect(SQL_HOST,SQL_USER,SQL_PASS,SQL_DB)
   or die('No se puedo conectar a MySql.' .mysql_error());
 
 
-
 //create the main database
 $tab = "create database wiley2";
 $x=mysql_query($tab)
  or die (mysql_error());
 //make sure our recently created database is the active one
 mysql_select_db ("wiley2");
-
-
 
 echo "Wiley2 Database successfully created!";
 
